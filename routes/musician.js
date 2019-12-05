fd11e2ead0a4a9d5bffea5e9dc1902b819666f0e
Samwise12 <sfelder1@binhamton.edu>
2019-12-05 13:00:02 -0500

    Add healthcheck route

--- routes/musician.js
+++ routes/musician.js
@@ -12,11 +12,7 @@ router.get('/health', (req, res) => {
 // retrieve all musicians from data store
 router.get('/all', (req, res) => {
   const { musician } = req.app.locals;
-  musician.getMusicians(req.params.id, (err, returnedMusicians) => {
-    if (err) {
-      res.status('400').send({errorMessage: err});
-    }
-    res.status('200').send(returnedMusicians);
+    res.status('200').send("200 - ok");
   });
 });
 
